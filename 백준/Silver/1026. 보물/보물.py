1ee4b60533d3c2eb2d857c7b2edde7e7257070bd
jaee55555 <jaeeO55555@gmail.com>
2024-07-10 10:09:32 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31120 KB -resolved

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -2,8 +2,10 @@ N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 A.sort()
-B.sort(reverse=True)
+# B.sort(reverse=True)
 total = 0
 for i in range(N):
-    total += A[i] * B[i]
+    max_b = max(B)
+    total += A[i] * max_b
+    B.pop(B.index(max_b))
 print(total)
