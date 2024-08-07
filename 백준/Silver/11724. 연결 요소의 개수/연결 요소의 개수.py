ad196ad2099e068eb0e8e86c910791d2cec5ff27
jaee55555 <102634972+jaee55555@users.noreply.github.com>
2024-08-07 19:15:51 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 636 ms, Memory: 65296 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,37 +1,27 @@
 import sys
 from collections import deque
-
+sys.setrecursionlimit(5000)
 N, M = map(int, sys.stdin.readline().split())
 li = [[] for i in range(N + 1)]
 visited = [False for i in range(N + 1)]
 cnt = 0
 
+def dfs(start, li, visited):
+    visited[start] = True
+    for t in li[start]:
+        if not visited[t]:
+            dfs(t, li, visited)
 
-def bfs(start, lli, is_visit):
-    temp = lli[start]
-    deq = deque()
-    for t in temp:
-        deq.append(t)
-    while deq:
-        left = deq.popleft()
-        temp = lli[left]
-        for t in temp:
-            if not is_visit[t]:
-                is_visit[t] = True
-                deq.append(t)
-    return is_visit
 
 
 for i in range(M):
     a, b = map(int, sys.stdin.readline().split())
     li[a].append(b)
     li[b].append(a)
-# print(li)
-# print(visited)
 
 for i in range(1, len(visited)):
     if not visited[i]:
-        visited = bfs(i, li, visited)
+        dfs(i, li, visited)
         cnt += 1
 
 print(cnt)
